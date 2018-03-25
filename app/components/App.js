e8ef0975ae21c36955d49d6b9c3f6ae81196a710
fouad <Chamma.fouad@gmail.com>
2018-03-25 20:09:01 +1100

    added ability to click on result and add to Saved list, updated CSS

--- app/components/App.js
+++ app/components/App.js
@@ -1,17 +1,46 @@
 import React from 'react'
 import Results from './Results'
+import _ from 'lodash'
+import './Properties.scss'
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
+    this.addProperty = this.addProperty.bind(this)
+    this.state = {
+      property: {
+        results: [],
+        saved: []
+      },
+    }
+  }
+
+  componentDidMount() {
+    let localDomain = 'http://localhost:3000/db'
+    fetch(`${localDomain}`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ property: res })
+      })
+  }
+
+  addProperty(index) {
+    var newProp = this.state.property.results[index]
+    console.log(newProp)
+    var arr = this.state.property
+    arr.saved.push(newProp)
+    this.setState({property: arr})
+
   }
 
   render() {
     return (
       <div>
-        <h1>REA Coding-Test</h1>
-        <Results />
+        <h1>Property Coding-Test</h1>
+        <Results  add={this.addProperty} status="Results" property={this.state.property.results}/>
+        <br/>
+        <Results className="saved" add={this.addProperty} status="Saved" property={this.state.property.saved} />
       </div>
     )      
   }
--- app/components/Results.js
+++ app/components/Results.js
@@ -7,70 +7,34 @@ export default class Results extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { 
-      results: [{
-        price: "$726,500",
-        agency: {
-          brandingColors: {
-            primary: "#ffe512"
-          },
-          logo: "http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif"
-        },
-        id: "1",
-        mainImage: "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
-      }, {
-        price: "$560,520",
-        agency: {
-          brandingColors: {
-            primary: "#fcfa3b"
-          },
-          logo: "http://i4.au.reastatic.net/agencylogo/BFERIC/12/20150619122858.gif"
-        },
-        id: "2",
-        mainImage: "http://i1.au.reastatic.net/640x480/88586227f9176f602d5c19cf06261108dbb29f03e30d1c4ce9fc2b51fb1e4bd6/main.jpg"
-      }, {
-        price: "$826,500",
-        agency: {
-          brandingColors: {
-            primary: "#57B5E0"
-          },
-          logo: "http://i1.au.reastatic.net/agencylogo/XCEWIN/12/20150807093203.gif"
-        },
-        id: "3",
-        mainImage: "http://i4.au.reastatic.net/640x480/98cee1b2a3a64329921fc38f7e2926a78d41fcc683fc48fb8a8ef2999b14c027/main.jpg"
-      }],
+    this.onSave = this.onSave.bind(this) 
+  }
 
-      saved: [{
-        price: "$526,500",
-        agency: {
-          brandingColors: {
-            primary: "#000000"
-          },
-          logo: "http://i2.au.reastatic.net/agencylogo/WVYSSK/2/20140701084436.gif"
-        },
-        id: "4",
-        mainImage: "http://i2.au.reastatic.net/640x480/5e84d96722dda3ea2a084d6935677f64872d1d760562d530c3cabfcb7bcda9c2/main.jpg"
-      }]
-    }
+  onSave(index) {
+    console.log("You're clicking Save")
+    var arr = this.props.add(index)
   }
 
   
-
   render() {
+    console.log(this.props.index)
+   const property = this.props.property
+  
     return (
       <table className="main">
-          <h1>Results</h1>
+          <h1>{this.props.status}</h1>
+          {console.log(property)}
         {
-          this.state.results.map((property, i) =>
+          property.map((property, index) =>
 
-            <tbody key={i}>
+            <tbody onClick={this.onSave.bind(this, index)} style={{ backgroundColor: property.agency.brandingColors.primary }} index={index} key={index}>
               <tr>
                 <td className="logo"><img src={property.agency.logo} /></td>
-                <td className="testColor"></td>
               </tr>
+              {console.log(index)}
 
               <tr>
-                <td><img src={property.mainImage} /></td>
+                <td><img src={property.mainImage} index={index} /></td>
               </tr>
 
               <tr>
@@ -80,31 +44,6 @@ export default class Results extends React.Component {
   
           )
         }
-
-        
-          <h1>Saved</h1>
-
-        {
-          this.state.saved.map((property, i) =>
-
-            <tbody key={i}>
-              <tr>
-                <td className="logo"><img src={property.agency.logo} /></td>
-                <td className="testColor"></td>
-              </tr>
-
-              <tr>
-                <td><img src={property.mainImage} /></td>
-              </tr>
-
-              <tr>
-                <td>{property.price}</td>
-              </tr>
-            </tbody>
-
-          )
-        }
-        
       </table>
     )      
   }
