2b4f76311d28940abf8645c73ac45d1fddae2d61
fouad <Chamma.fouad@gmail.com>
2018-03-30 19:20:15 +1100

    added test to show removeProperty function

--- app/components/Results.test.js
+++ app/components/Results.test.js
@@ -47,8 +47,6 @@ describe('Results Component', () => {
 		const addFunction = wrapper.find('Results').prop('add')
 		const index = 0
 
-		console.log(addFunction)
-
 		addFunction(index)
 
 		const expectedSaved = [results[index]]
@@ -56,5 +54,40 @@ describe('Results Component', () => {
 		const actualSaved = wrapper.state().property.saved
 
 		expect(actualSaved).toEqual(expectedSaved)
+	})
+
+	it('Function remove property from saved state', () => {
+		const wrapper = shallow(<App name="Fouad" />)
+
+		const sampleProperty = {
+			"price": "$726,500",
+			"agency": {
+				"brandingColors": {
+					"primary": "#ffe512"
+				},
+				"logo": "http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif"
+			},
+			"id": "1",
+			"mainImage": "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
+		}
+
+		wrapper.setState({
+			property: {
+				results: [],
+				saved: [sampleProperty]
+			}
+		})
+
+		const index = 0
+		const results = wrapper.state().property.saved
+		const removeFunction = wrapper.find('Saved').prop('remove')
+		const expectedSaved = [results[index]]
+
+		removeFunction(index)
+
+		expect(results).toEqual([])
+
+
+
 	})
 })
