2eb27fc4836da877a175babb4e684348f2a461eb
fouad <Chamma.fouad@gmail.com>
2018-03-30 12:43:41 +1100

    added test to show addProperty function

--- app/components/Results.test.js
+++ app/components/Results.test.js
@@ -22,8 +22,39 @@ describe('Results Component', () => {
 })
 
 describe('Results Component', () => {
-	it('Function Remove property from saved state', () => {
-		const Component = shallow(<App name="Fouad" />)
+	it('Function add property to saved state', () => {
+		const wrapper = shallow(<App name="Fouad" />)
+		const sampleProperty = {
+			"price": "$726,500",
+			"agency": {
+				"brandingColors": {
+					"primary": "#ffe512"
+				},
+				"logo": "http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif"
+			},
+			"id": "1",
+			"mainImage": "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
+		}
+
+		wrapper.setState({
+			property: {
+				results: [sampleProperty],
+				saved: []
+			}
+		})
+
+		const results = wrapper.state().property.results
+		const addFunction = wrapper.find('Results').prop('add')
+		const index = 0
+
+		console.log(addFunction)
+
+		addFunction(index)
+
+		const expectedSaved = [results[index]]
+
+		const actualSaved = wrapper.state().property.saved
+
+		expect(actualSaved).toEqual(expectedSaved)
 	})
 })
-
